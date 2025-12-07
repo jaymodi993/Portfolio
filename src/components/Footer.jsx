3a02c377af188b98cfe4119ef77a1915c87ff238
Jay Modi <jaymodi993@gmail.com>
2025-12-07 17:17:58 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,15 @@
 const Footer = () => (
-  <footer className="footer-like-navbar">
+  <footer>
     <div className="footer-container">
       
-      {/* 1. Logo Section (Matches Navbar) */}
+      {/* Logo Section */}
       <div className="footer-logo">
         <a href="#home" aria-label="Back to Top">
           <img src="/img/Jay's Portfolio_transparent.png" alt="Logo" loading="lazy" />
         </a>
       </div>
 
-      {/* 2. Navigation Links (Horizontal, like Navbar) */}
-      <ul className="footer-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#aboutme">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#service">Services</a></li>
-        <li><a href="#project">Projects</a></li>
-      </ul>
-
-      {/* 3. Social Icons (Styled simply) */}
+      {/* Social Icons Section (Links Removed) */}
       <div className="footer-socials">
         <a href="https://www.linkedin.com/in/jay-modi-60965519a/" target="_blank" aria-label="LinkedIn"><i className="fa-brands fa-linkedin"></i></a>
         <a href="https://twitter.com/jaymodi993" target="_blank" aria-label="Twitter"><i className="fa-brands fa-x-twitter"></i></a>
